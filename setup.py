eb659169caf28ce3bf28fb62d4b2f828726aeb11
Adam Bosak <bosadam@seznam.cz>
2022-08-18 10:06:23 +0200

    improved setup

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-from setuptools import find_packages
 import os
-from setuptools import setup, Command
+from setuptools import setup, find_packages, Command
 
 
 class CleanCommand(Command):
@@ -21,7 +20,8 @@ setup(
 	author='Bosak Adam',
 	author_email='bosadam@seznam.cz',
 	url='https://github.com/bosakad/GP-Optimization',
-	packages=find_packages(),
+	#packages=find_packages(),
+	packages=setuptools.find_namespace_packages(exclude=["tests", "docs","examples","inputs_outputs"]),
 	cmdclass={
         'clean': CleanCommand,
     }
